261c559b29b211327b93bb3ee3d7641c2a5dbb41
Reasonlesss <28310208+Reasonlesss@users.noreply.github.com>
2022-10-06 20:19:12 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unflipper Bot
+A discord bot that prevents those tables from getting abused in your discord server. It's not hard people... Stop abusing tables :(
---
+++ src/main/java/codes/reason/UnflipperBot.java
@@ -0,0 +1,74 @@
+package codes.reason;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
+
+// Made to automatically unflip, flipped tables.
+// Tables do not deserve the hate that they must
+// endure from evil people.
+public class UnflipperBot {
+
+    private static final String FLIP_STRING = "(╯°□°）╯︵ ┻━┻";
+    private static final String UNFLIP_STRING = "┬─┬ ノ( ゜-゜ノ)";
+
+    public static void main(String[] args) {
+        // Whoever is running the bot forgot the token in the
+        // launch arguments and should probably be shot on the spot.
+        if (args.length == 0) {
+            System.err.println("Someone forgot to provide a bot token, pretty stupid tbh");
+            System.exit(0);
+            return;
+        }
+
+        final String token = args[0];
+
+        // Create the actual bot instance
+        final JDA jda = JDABuilder.createLight(token)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
+
+        // Listen for potential abuse and correct things :)
+        jda.addEventListener(new ListenerAdapter() {
+            @Override
+            public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+                final String message = event.getMessage().getContentRaw();
+                int flips = isFlipped(message);
+                if (flips > 0) {
+                    String repeated = ("  " + UNFLIP_STRING).repeat(flips).substring(1);
+                    event.getMessage().reply(repeated).queue();
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Checks if a poor table has been flipped over, due to people
+     * abusing MULTIPLE tables in the same message (unacceptable) I have
+     * decided to correct multiple unflipped tables.
+     */
+    private static int isFlipped(String content) {
+        boolean flip = true;
+        int totalOccurrences = 0;
+
+        while (flip) {
+            for (char c : FLIP_STRING.toCharArray()) {
+                String charAsString = String.valueOf(c);
+                if (content.contains(charAsString)) {
+                    content = content.replaceFirst(Pattern.quote(charAsString), "");
+                }else {
+                    flip = false;
+                }
+            }
+            if (flip) totalOccurrences++;
+        }
+        return totalOccurrences;
+    }
+
+}
